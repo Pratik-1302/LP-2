61f2a1a6e44cd3cda56e73513618cc9868dcfdf0
Pratik Kape <119004904+Pratik-1302@users.noreply.github.com>
2024-04-13 02:10:29 +0530

    Create t4.py

---
+++ t4.py
@@ -0,0 +1,10 @@
+# Function to add two numbers
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calling the function and printing the result
+print("The sum is:", add_numbers(num1, num2))
